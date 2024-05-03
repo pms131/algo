52fa133cf4d8089610d2b5536c83e4ccaaf12c2b
박문수 <sportssk@naver.com>
2024-05-03 14:53:40 +0900

    [Algorithm] 삽입 정렬
     - 삽입 정렬 구현

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -13,24 +13,24 @@ import java.util.ArrayList;
 public class InsertSort extends Sort {
 
 	public InsertSort() {
-		System.out.println("## 선택 정렬 시작 ##");
+		System.out.println("## 삽입 정렬 시작 ##");
 	}
 
 	@Override
 	protected ArrayList<Integer> doSort(ArrayList<Integer> array) {
+		for (int i = 1; i < array.size(); i++) {
+			// 선택 데이터
+			int key = array.get(i);
+			//비교 데이터
+			int j = i - 1;
 
-		for (int i = 0; i < array.size() - 1; i++) {
-			int tmp = array.get(i + 1);
-			for (int j = i + 1; j >= 0; j--) {
-				if (j == 0 || array.get(j - 1) <= tmp) {
-					array.set(j, tmp);
-					break;
-				} else {
-					array.set(j, array.get(j - 1));
-				}
+			// 이전의 원소가 더 크다면 데이터는 하나씩 밀려난다.
+			while (j >= 0 && array.get(j) > key) {
+				array.set(j + 1, array.get(j));
+				j--;
 			}
+			array.set(j + 1, key);
 		}
-
 		return array;
 	}
 
